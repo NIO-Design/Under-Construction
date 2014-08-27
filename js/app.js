437d69d3005bf9bcf6547f3595dde4afc007cd0c
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2014-08-27 05:59:40 +0300

    Simple refactoring.
    - updates -

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  
+  function mainViewModel() {
+
+    var header = "Страницата е в процес на разработка.",
+        topic = "До откриването на новата версия остават:",
+        days = "Дни",
+		hours = "Часове",
+        minutes = "Минути",
+        seconds = "Секунди"; 
+
+    this.baseLine = ko.observable(header);
+    this.secondLine = ko.observable(topic);
+	this.days = ko.observable(days);
+	this.hours = ko.observable(hours);
+	this.minutes = ko.observable(minutes);
+	this.seconds = ko.observable(seconds);
+  };
+
+  ko.applyBindings(new mainViewModel()); 
+
+});
